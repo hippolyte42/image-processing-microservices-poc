50709e00339fcc9d48527c37860c0e66ca58a873
hippolyte42 <hippolyte.jacque@gmail.com>
2024-12-04 16:01:11 +0100

    feat(service2): process to blur image

--- service2/index.js
+++ service2/index.js
@@ -1,6 +1,36 @@
 const express = require("express");
+const sharp = require("sharp");
 
 const app = express();
 const PORT = 3002;
+
+app.get("/process/:imageFileName", async (req, res) => {
+  try {
+    const { imageFileName } = req.params;
+    const imageFileExtension = imageFileName.split(".").pop();
+
+    const response = await fetch(
+      `http://service1:3001/image/${imageFileName}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/octet-stream",
+        },
+      }
+    );
+    const imageDataArrayBuffer = await response.arrayBuffer();
+
+    const blurredImage = await sharp(imageDataArrayBuffer).blur(10).toBuffer();
+
+    res.set("Content-Type", `image/${imageFileExtension}`);
+    res.send(blurredImage);
+  } catch (err) {
+    res
+      .status(500)
+      .send(
+        `Error while processing image ${imageFileName} : ${JSON.stringify(err)}`
+      );
+  }
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
