2f7552148f672061d6e0e8d91cbfa8ad3eabc298
hippolyte42 <hippolyte.jacque@gmail.com>
2024-12-04 15:42:51 +0100

    feat(service1): upload image

--- service1/index.js
+++ service1/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
+const upload = multer({ dest: "cache/" });
 const PORT = 3001;
+
+app.post("/image", upload.single("image"), (req, res) => {
+  const tmpPath = req.file.path;
+  const targetPath = path.join(__dirname, "cache", req.file.originalname);
+
+  fs.rename(tmpPath, targetPath, (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .send(`Error while uploading image: ${JSON.stringify(err)}`);
+    }
+    res.send("Image upload succesfull");
+  });
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
